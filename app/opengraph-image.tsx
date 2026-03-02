dfe74a70d545e204ac6ff9f9dcaa162784623aa0
sainsw <s.ainsworth@me.com>
2026-03-02 14:22:46 +0000

    removed edge runtime vercel due to outage

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from 'next/og';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export const alt = 'Invoicer social preview';
 
 export default async function Image() {
